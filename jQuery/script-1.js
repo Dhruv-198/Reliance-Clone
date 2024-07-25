4e964cac94869cf459594920ce069b2c0916a16a
Dhruv-198 <rikipatel1908@gmail.com>
2024-07-25 15:08:17 +0530

    first commit

---
+++ jQuery/script-1.js
@@ -0,0 +1,331 @@
+
+$(document).ready(function () {
+  $("#L1").mouseover(function () {
+    $(".submenu").css({ display: "block" });
+    $(".submenu1").css({ display: "none" });
+    $(".submenu2").css({ display: "none" });
+  });
+  $("#L2").mouseover(function () {
+    $(".submenu1").css({ display: "block" });
+    $(".submenu").css({ display: "none" });
+    $(".submenu2").css({ display: "none" });
+  });
+  $("#L3").mouseover(function () {
+    $(".submenu2").css({ display: "block" });
+    $(".submenu").css({ display: "none" });
+    $(".submenu1").css({ display: "none" });
+  });
+  $(".main-1").mouseover(function () {
+    $(".submenu2").css({ display: "none" });
+    $(".submenu").css({ display: "none" });
+    $(".submenu1").css({ display: "none" });
+  });
+});
+
+// CONTACT-US 
+$(".contact").mouseover(function () {
+  $(".submenu2").css({ display: "none" });
+  $(".submenu").css({ display: "none" });
+  $(".submenu1").css({ display: "none" });
+});
+
+// CARREES
+$(".career").mouseover(function () {
+  $(".submenu2").css({ display: "none" });
+  $(".submenu").css({ display: "none" });
+  $(".submenu1").css({ display: "none" });
+});
+// URBAN
+$(".urban").mouseover(function () {
+  $(".submenu2").css({ display: "none" });
+  $(".submenu").css({ display: "none" });
+  $(".submenu1").css({ display: "none" });
+});
+// GALLARY
+$(".gallary").mouseover(function () {
+  $(".submenu2").css({ display: "none" });
+  $(".submenu").css({ display: "none" });
+  $(".submenu1").css({ display: "none" });
+});
+// EVENTS 
+
+$(document).ready(function () {
+  $(".p1").click(function () {
+    $("#hide-1").slideToggle()
+    $("#m4-img1").show()
+    $("#m4-img2").hide()
+    $("#m4-img3").hide()
+    $("#m4-img4").hide()
+    $("#m4-img5").hide()
+    $("#m4-img6").hide()
+    $("#m4-img7").hide()
+    $("#m4-img8").hide()
+    $("#m4-img9").hide()
+    $("#m4-img10").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-2").slideUp()
+    $("#hide-10").slideUp()
+    $("#hide-3").slideUp()
+    $("#hide-4").slideUp()
+    $("#hide-5").slideUp()
+    $("#hide-6").slideUp()
+    $("#hide-7").slideUp()
+    $("#hide-8").slideUp()
+    $("#hide-9").slideUp()
+    
+  })
+  $(".p2").click(function () {
+    $("#hide-2").slideToggle()
+    $("#m4-img2").show()
+    $("#m4-img1").hide()
+    $("#m4-img3").hide()
+    $("#m4-img4").hide()
+    $("#m4-img5").hide()
+    $("#m4-img6").hide()
+    $("#m4-img7").hide()
+    $("#m4-img8").hide()
+    $("#m4-img9").hide()
+    $("#m4-img10").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-1").slideUp()
+    $("#hide-10").slideUp()
+    $("#hide-3").slideUp()
+    $("#hide-4").slideUp()
+    $("#hide-5").slideUp()
+    $("#hide-6").slideUp()
+    $("#hide-7").slideUp()
+    $("#hide-8").slideUp()
+    $("#hide-9").slideUp()
+   
+  })
+  $(".p3").click(function () {
+    $("#hide-3").slideToggle()
+    $("#m4-img3").show()
+    $("#m4-img1").hide()
+    $("#m4-img2").hide()
+    $("#m4-img4").hide()
+    $("#m4-img5").hide()
+    $("#m4-img6").hide()
+    $("#m4-img7").hide()
+    $("#m4-img8").hide()
+    $("#m4-img9").hide()
+    $("#m4-img10").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-1").slideUp()
+    $("#hide-10").slideUp()
+    $("#hide-2").slideUp()
+    $("#hide-4").slideUp()
+    $("#hide-5").slideUp()
+    $("#hide-6").slideUp()
+    $("#hide-7").slideUp()
+    $("#hide-8").slideUp()
+    $("#hide-9").slideUp()
+   
+  })
+  $(".p4").click(function () {
+    $("#hide-4").slideToggle()
+    $("#m4-img4").show()
+    $("#m4-img1").hide()
+    $("#m4-img2").hide()
+    $("#m4-img3").hide()
+    $("#m4-img5").hide()
+    $("#m4-img6").hide()
+    $("#m4-img7").hide()
+    $("#m4-img8").hide()
+    $("#m4-img9").hide()
+    $("#m4-img10").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-1").slideUp()
+    $("#hide-10").slideUp()
+    $("#hide-3").slideUp()
+    $("#hide-2").slideUp()
+    $("#hide-5").slideUp()
+    $("#hide-6").slideUp()
+    $("#hide-7").slideUp()
+    $("#hide-8").slideUp()
+    $("#hide-9").slideUp()
+    
+  })
+  $(".p5").click(function () {
+    $("#hide-5").slideToggle()
+    $("#m4-img5").show()
+    $("#m4-img1").hide()
+    $("#m4-img2").hide()
+    $("#m4-img3").hide()
+    $("#m4-img4").hide()
+    $("#m4-img6").hide()
+    $("#m4-img7").hide()
+    $("#m4-img8").hide()
+    $("#m4-img9").hide()
+    $("#m4-img10").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-1").slideUp()
+    $("#hide-10").slideUp()
+    $("#hide-3").slideUp()
+    $("#hide-4").slideUp()
+    $("#hide-2").slideUp()
+    $("#hide-6").slideUp()
+    $("#hide-7").slideUp()
+    $("#hide-8").slideUp()
+    $("#hide-9").slideUp()
+  
+  })
+  $(".p6").click(function () {
+    $("#hide-6").slideToggle()
+    $("#m4-img6").show()
+    $("#m4-img1").hide()
+    $("#m4-img2").hide()
+    $("#m4-img3").hide()
+    $("#m4-img4").hide()
+    $("#m4-img5").hide()
+    $("#m4-img7").hide()
+    $("#m4-img8").hide()
+    $("#m4-img9").hide()
+    $("#m4-img10").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-1").slideUp()
+    $("#hide-10").slideUp()
+    $("#hide-3").slideUp()
+    $("#hide-4").slideUp()
+    $("#hide-5").slideUp()
+    $("#hide-2").slideUp()
+    $("#hide-7").slideUp()
+    $("#hide-8").slideUp()
+    $("#hide-9").slideUp()
+   
+  })
+  $(".p7").click(function () {
+    $("#hide-7").slideToggle()
+    $("#m4-img7").show()
+    $("#m4-img1").hide()
+    $("#m4-img2").hide()
+    $("#m4-img3").hide()
+    $("#m4-img4").hide()
+    $("#m4-img5").hide()
+    $("#m4-img6").hide()
+    $("#m4-img8").hide()
+    $("#m4-img9").hide()
+    $("#m4-img10").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-1").slideUp()
+    $("#hide-10").slideUp()
+    $("#hide-3").slideUp()
+    $("#hide-4").slideUp()
+    $("#hide-5").slideUp()
+    $("#hide-6").slideUp()
+    $("#hide-2").slideUp()
+    $("#hide-8").slideUp()
+    $("#hide-9").slideUp()
+   
+  })
+  $(".p8").click(function () {
+    $("#hide-8").slideToggle()
+    $("#m4-img8").show()
+    $("#m4-img1").hide()
+    $("#m4-img2").hide()
+    $("#m4-img3").hide()
+    $("#m4-img4").hide()
+    $("#m4-img5").hide()
+    $("#m4-img6").hide()
+    $("#m4-img7").hide()
+    $("#m4-img9").hide()
+    $("#m4-img10").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-1").slideUp()
+    $("#hide-10").slideUp()
+    $("#hide-3").slideUp()
+    $("#hide-4").slideUp()
+    $("#hide-5").slideUp()
+    $("#hide-6").slideUp()
+    $("#hide-7").slideUp()
+    $("#hide-2").slideUp()
+    $("#hide-9").slideUp()
+    
+  })
+  $(".p9").click(function () {
+    $("#hide-9").slideToggle()
+    $("#m4-img9").show()
+    $("#m4-img1").hide()
+    $("#m4-img2").hide()
+    $("#m4-img3").hide()
+    $("#m4-img4").hide()
+    $("#m4-img5").hide()
+    $("#m4-img6").hide()
+    $("#m4-img7").hide()
+    $("#m4-img8").hide()
+    $("#m4-img10").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-1").slideUp()
+    $("#hide-10").slideUp()
+    $("#hide-3").slideUp()
+    $("#hide-4").slideUp()
+    $("#hide-5").slideUp()
+    $("#hide-6").slideUp()
+    $("#hide-7").slideUp()
+    $("#hide-8").slideUp()
+    $("#hide-2").slideUp()
+    
+  })
+  $(".p10").click(function () {
+    $("#hide-10").slideToggle()
+    $("#m4-img10").show()
+    $("#m4-img1").hide()
+    $("#m4-img2").hide()
+    $("#m4-img3").hide()
+    $("#m4-img4").hide()
+    $("#m4-img5").hide()
+    $("#m4-img6").hide()
+    $("#m4-img7").hide()
+    $("#m4-img8").hide()
+    $("#m4-img9").hide()
+    $(".main-4").toggleClass("one")
+    $("#hide-1").slideUp()
+    $("#hide-2").slideUp()
+    $("#hide-3").slideUp()
+    $("#hide-4").slideUp()
+    $("#hide-5").slideUp()
+    $("#hide-6").slideUp()
+    $("#hide-7").slideUp()
+    $("#hide-8").slideUp()
+    $("#hide-9").slideUp()
+  })
+})
+
+$(document).ready(function () {
+    $("#ic1").mouseover(function () {
+        $("#in1").toggle()
+        $("#in2").hide()
+        $("#in3").hide()
+        $("#in4").hide()
+        $("#in5").hide()
+    })
+    $("#ic2").mouseover(function () {
+        $("#in2").toggle()
+        $("#in1").hide()
+        $("#in3").hide()
+        $("#in4").hide()
+        $("#in5").hide()
+    })
+    $("#ic3").mouseover(function () {
+        $("#in3").toggle()
+        $("#in1").hide()
+        $("#in2").hide()
+        $("#in4").hide()
+        $("#in5").hide()
+    })
+    $("#ic4").mouseover(function () {
+        $("#in4").toggle()
+        $("#in1").hide()
+        $("#in2").hide()
+        $("#in3").hide()
+        $("#in5").hide()
+    })
+    $("#ic5").mouseover(function () {
+        $("#in5").toggle()
+        $("#in1").hide()
+        $("#in3").hide()
+        $("#in4").hide()
+        $("#in2").hide()
+    })
+})
+
